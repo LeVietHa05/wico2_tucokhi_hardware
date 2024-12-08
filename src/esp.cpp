9a17cfed03a44fc89425b83542c221fe5b51429d
LeVietHa05 <ha.leviet.0504@gmail.com>
2024-12-08 13:17:05 +0700

    fix error in sensor

--- src/esp.cpp
+++ src/esp.cpp
@@ -7,9 +7,9 @@
 // #include <WiFiManager.h>
 // #include <SPI.h>
 // #include <MFRC522.h>
-
-// Đây là code cho esp
-
+// 
+// // Đây là code cho esp
+// 
 // //====================================================
 // #define MEGA_RX D1
 // #define MEGA_TX D2
@@ -32,7 +32,8 @@
 // int id = 0;
 // int Floor = 0;
 // byte nuidPICC[4];
-// unsigned long uidDec, uidDecTemp; // hien thi so UID dang thap phan
+// unsigned long uidDec, uidDecTemp, lastReadCard; // hien thi so UID dang thap phan
+// bool isUIDclear = false;
 // //====================================================
 // void socketIOEvent(socketIOmessageType_t type, uint8_t *payload, size_t length)
 // {
@@ -124,7 +125,7 @@
 //         Serial.print(buffer[i], HEX);
 //     }
 // }
-//
+// 
 // /**
 //    Helper routine to dump a byte array as dec values to Serial.
 // */
@@ -136,7 +137,7 @@
 //         Serial.print(buffer[i], DEC);
 //     }
 // }
-//
+// 
 // //====================================================
 // void setup()
 // {
@@ -197,7 +198,7 @@
 //             sendDataPosOfThing(msg);
 //         }
 //     }
-//
+// 
 //     // check rfid
 //     if (!rfid.PICC_IsNewCardPresent())
 //     {
@@ -205,46 +206,41 @@
 //         // mega_Serial.println("Lock");
 //         return;
 //     }
-//
+// 
 //     // Verify if the NUID has been readed
 //     if (!rfid.PICC_ReadCardSerial())
 //         return;
-//
+// 
 //     Serial.print(F("PICC type: "));
 //     MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
 //     Serial.println(rfid.PICC_GetTypeName(piccType));
-//
+// 
 //     // Check is the PICC of Classic MIFARE type
-//     if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
-//         piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-//         piccType != MFRC522::PICC_TYPE_MIFARE_4K)
+//     if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI && piccType != MFRC522::PICC_TYPE_MIFARE_1K && piccType != MFRC522::PICC_TYPE_MIFARE_4K)
 //     {
 //         Serial.println(F("Your tag is not of type MIFARE Classic."));
 //         return;
 //     }
-//
+// 
 //     // if the card is new, send data to server, else do nothing
-//     if (rfid.uid.uidByte[0] != nuidPICC[0] ||
-//         rfid.uid.uidByte[1] != nuidPICC[1] ||
-//         rfid.uid.uidByte[2] != nuidPICC[2] ||
-//         rfid.uid.uidByte[3] != nuidPICC[3])
+//     if (rfid.uid.uidByte[0] != nuidPICC[0] || rfid.uid.uidByte[1] != nuidPICC[1] || rfid.uid.uidByte[2] != nuidPICC[2] || rfid.uid.uidByte[3] != nuidPICC[3])
 //     {
 //         uidDec = 0;
 //         Serial.println(F("A new card has been detected."));
-//
+// 
 //         // Store NUID into nuidPICC array
 //         for (byte i = 0; i < 4; i++)
 //         {
 //             nuidPICC[i] = rfid.uid.uidByte[i];
 //         }
-//
+// 
 //         for (byte i = 0; i < rfid.uid.size; i++)
 //         {
 //             uidDecTemp = rfid.uid.uidByte[i];
 //             uidDec = uidDec * 256 + uidDecTemp;
 //         }
 //         Serial.println(uidDec);
-//
+// 
 //         Serial.println(F("The NUID tag is:"));
 //         Serial.print(F("In hex: "));
 //         printHex(rfid.uid.uidByte, rfid.uid.size);
@@ -252,15 +248,23 @@
 //         Serial.print(F("In dec: "));
 //         printDec(rfid.uid.uidByte, rfid.uid.size);
 //         Serial.println();
-//
+//         isUIDclear = false;
 //         sentDataWhenCardFound();
 //     }
 //     else
 //         Serial.println(F("Card read previously."));
-//
+// 
+//     if (millis() - lastReadCard > 15000 && !isUIDclear)
+//     {
+//         for (byte i = 0; i < 4; i++)
+//         {
+//             nuidPICC[i] = 0;
+//         }
+//         isUIDclear = true;
+//     }
 //     // Halt PICC
 //     rfid.PICC_HaltA();
-//
+// 
 //     // Stop encryption on PCD
 //     rfid.PCD_StopCrypto1();
 // }
